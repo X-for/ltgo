e95830b22af5e6d6def8cb7a29238dd607fb7630
x-for <zaq021004@163.com>
2026-02-06 12:21:34 +0800

    add config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,3 +4,38 @@ import (
 	"path/filepaht"
 )
 
+
+type ConfigInfo struct {
+	cookie string
+	language string
+	url string
+}
+
+type Config struct {
+	path string
+	cfg ConfigInfo
+}
+
+
+func getConfigPath() map[string]string {
+	path = os.UserHomeDir()
+	dir, err := os.ReadDir(path + ".ltgo")
+	if err != nil {
+		log.Fetal(err)
+		err := os.Mkdir(".ltgo", 0755)
+		if err != nil {
+			log.Fetal(err)
+			_, err := Create(dir + "config.json")
+		}
+	}
+	file, err := os.ReadFile(dir + "config.json")
+	if err != nil {
+		log.Fetal(err)
+		file, err := Create(file + "config.json")
+		if err != nil {
+			log.Fetal(err)
+		}
+	}
+}
+
+func Load(
